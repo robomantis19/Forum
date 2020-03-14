81129295e19d943333f3b92d7a1e76c3a3f88c72
robomantis19 <freefavors@gmail.com>
2020-03-14 19:40:03 -0400

    added pull

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -4,7 +4,7 @@ export const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
 
   return axios.create({
-    baseURL: "forumbusters.us-west-2.elasticbeanstalk.com/api/users",
+    baseURL: "http://www.forumbusters.us-west-2.elasticbeanstalk.com/api/users",
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `${token}`
